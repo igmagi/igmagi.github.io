a3206fccce159ba9bc3de0b68a92c1a0b915b553
igmagi <ignasimariginer@gmail.com>
2022-12-29 08:16:10 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,156 @@
+const initMenuScroll = () => {
+        var c,
+        currentScrollTop = 0,
+        navbar = document.querySelector("nav");
+
+        document.addEventListener("scroll", function () {
+            var a = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+            var b = navbar.offsetHeight;
+
+            currentScrollTop = a;
+
+            if (c < currentScrollTop && a > b + b) {
+                navbar.classList.add("scrollUp");
+            } else if (c > currentScrollTop && !(a <= b)) {
+                navbar.classList.remove("scrollUp");
+            }
+            c = currentScrollTop;
+
+        });
+};
+
+// const initAboutAnim = () => {
+//     let about = document.querySelector("#about");
+//     let aboutbg = document.querySelector("#about-bg");
+//     document.addEventListener("scroll", function () {
+//         var a = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+//         var b = about.offsetHeight;
+
+//         currentScrollTop = a;
+//         aboutbg.style.left = `-${(currentScrollTop/b)*100 * 1.5}%`
+//     });
+// }
+
+class toC {
+    constructor(){
+        this.l = undefined;
+    }
+    save(to) {
+        if(this.l) clearTimeout(this.l) 
+        this.l = to;
+    }
+}
+// const initEducationExplanations = () => {
+//     const expls = {
+//         fp: document.querySelector("#fp-explanation"),
+//         it: document.querySelector("#it-explanation") 
+//     }
+//     let toc = new toC();
+//     const links = document.querySelectorAll(".expl-link");
+//     links.forEach(link => {
+//     //     link.addEventListener("mouseover", (ev)=>{
+//     //         expls[targetId].style.display = "block";
+
+//     // })
+//     //     link.addEventListener("mouseout", (ev)=>{
+//     //         expls[targetId].style.display = "none";
+
+//     // })
+//         link.addEventListener("click", (ev)=>{
+//             targetId = ev.currentTarget.id
+//             if(expls[targetId].classList.contains("animate-explanation")){
+//                 expls["fp"].classList.remove("animate-explanation");
+//                 expls["it"].classList.remove("animate-explanation");
+//                 return;
+//             }
+
+//             expls["fp"].classList.remove("animate-explanation");
+//             expls["it"].classList.remove("animate-explanation");
+//             expls[targetId].classList.add("animate-explanation");
+
+//             toc.save(setTimeout(()=>{
+//                 expls[targetId].classList.remove("animate-explanation")
+//             },7000))
+//         })
+//     })
+    
+
+// }
+
+const initProjects = () => {
+    console.log(projects)
+    const projWrapper = document.querySelector("#projects-wrapper");
+    projects.map((pr)=>{
+        var node_0 = document.createElement("A");
+        node_0.setAttribute('href', pr?.links?.live ? pr?.links?.live : pr?.links?.git);
+        node_0.setAttribute("target", "_blank")
+        
+        var node_1 = document.createElement('DIV');
+        node_1.setAttribute('class', 'project');
+
+        node_0.appendChild(node_1);
+
+        var node_2 = document.createElement('DIV');
+        node_2.setAttribute('class', 'project-title');
+        node_1.appendChild(node_2);
+
+        var node_3 = document.createTextNode((new String(pr.title)));
+        node_2.appendChild(node_3);
+
+        var node_4 = document.createElement('DIV');
+        node_4.setAttribute('class', 'project-description');
+        node_1.appendChild(node_4);
+
+        var node_5 = document.createTextNode((new String(pr.description)));
+        node_4.appendChild(node_5);
+
+        var node_6 = document.createElement('DIV');
+        node_6.setAttribute('class', 'project-labels');
+        node_1.appendChild(node_6);
+
+        pr.tags.map(lab=>{
+            var node_7 = document.createElement('SPAN');
+            node_6.appendChild(node_7);
+
+            var node_8 = document.createTextNode((new String(lab)));
+            node_7.appendChild(node_8);
+        })
+
+        projWrapper.appendChild(node_0)
+    })
+}
+const initCopyListeners = () => {
+    let tlph = document.querySelector(".phone");
+    let email = document.querySelector(".email");
+
+    tlph.addEventListener("click", handleCopy)
+    email.addEventListener("click", handleCopy)
+
+}
+
+const handleCopy = (ev)=>{
+    let toc = new toC()
+    let tt = document.querySelector(".tooltip");
+    if(tt.classList.contains("animate-tool")){
+        tt.classList.remove("animate-tool");
+        return;
+    }
+
+    tt.style.top = `${ev.pageY}px`
+    tt.style.left = `${ev.pageX}px`
+    tt.classList.add("animate-tool");
+
+    toc.save(setTimeout(()=>{
+        tt.classList.remove("animate-tool")
+    },1500))
+    
+    navigator.clipboard.writeText(ev.target.getAttribute("cp-data"));
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    initProjects();
+    initMenuScroll();
+    //initAboutAnim();
+    initCopyListeners();
+    // initEducationExplanations();
+})
---
+++ projects.js
@@ -0,0 +1,112 @@
+const projects = [
+    {
+        title: "qyde: qué y dónde estudiar",
+        description:
+            "Web app for soon-to-be students to find their future student flat, roommates and classmates in Spain.\nFilter the posts by location, university and degree and forget about having to resort to social media to find accomodation or colleagues.",
+        tags: [
+            "react",
+            "redux",
+            "nodejs",
+            "mongodb",
+            "express",
+            "websocket",
+            "jwt",
+        ],
+        links: {
+            git: "",
+            live: "wee"
+        },
+        img_url: "",
+    },
+    {
+        title: "ummo11: the experience",
+        description:
+            "You become stranded on Mars after the spaceship you were travelling in suffered an accident. Recollect the pieces that broke and fix the aircraft to escape this 'deserted' planet.\nRide the spacial bike and weld the broken parts back to the ship in this VR experience. ",
+        tags: [
+            "unity", 
+            "c#", 
+            "maya", 
+            "vrkit"
+            ],
+        links: {
+            git: "",
+        },
+        img_url: "",
+    },
+    {
+        title: "rito: the robotic shopping cart",
+        description:
+            "A smart shopping cart that optimizes the path that buyers take when buying.\nUsers create their shopping list on the Rito app at home and link their app to the cart. Rito will create the optimal path and displays the map of the current store along with the user's exact location.",
+        tags: [
+            "feathersjs",
+            "mongodb",
+            "python",
+            "nodejs",
+        ],
+        links: {
+            git: "",
+        },
+        img_url: "",
+    },
+    {
+        title: "trackair: smart city sensoring",
+        description:
+            "IoT device that measures the amount of irritant gases. It sends data via bluetooth to the user's phone, which alerts the user in case of high levels of these fumes.\nThese measurements are sent to the server which are processed to build a heat map using interpolation. There's a web client to access the data and the map.",
+        tags: [
+            "express",
+            "android",
+            "mysql",
+            "c++",
+        ],
+        links: {
+            git: "",
+        },
+        img_url: "",
+    },
+    {
+        title: "cookin",
+        description:
+            "Intelligent extractor hood equipped with a thermal camera to have full control of what is being cooked. Anti-smoke system which turns on the hood if needed.\nCookin comes with a recipe app that can be used to monitor the kitchen and to control the lights and the hood.",
+        tags: [
+            "android",
+            "firebase",
+            "arduino",
+            "c++",
+        ],
+        links: {
+            git: "https://github.com/Grupo01IoT/CocinaInteligente",
+        },
+        img_url: "",
+    },
+    {
+        title: "tyro: the game",
+        description:
+            "Our hero, Tyro, has to retrieve the gems. He will have to fight enemies using his agility and his magic in order to complete his mission.\n Have some fun in this fast-paced platform by slowing down time with Tyro's powers.",
+        tags: [
+            "unity", 
+            "c#",
+            "3dsmax", 
+            "photoshop", 
+            ],
+        links: {
+            git: "",
+            live: "https://do-videogames.webs.upv.es/juegos/tyro/"
+        },
+        img_url: "",
+    },
+    {
+        title: "iot farmland sensor",
+        description:
+            "IoT device that measures humidity, salinity, sunlight, pressure, temperature, gps coordinates and has a built-in anti-theft mechanism.\nAll data is sent to a server using their API. Users can track their farmlands through a web application.",
+        tags: [
+            "c++", 
+            "electronics", 
+            "arduino", 
+            "API"
+            ],
+        links: {
+            git: "https://github.com/igmagi/IoT-farmland",
+        },
+        img_url: "",
+    },
+];
